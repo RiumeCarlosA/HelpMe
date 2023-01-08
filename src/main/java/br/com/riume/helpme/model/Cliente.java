fea036d0553e4e730a1f522a654c6ab60f577915
RiumeCarlosA <riumecarlos@hotmail.com>
2023-01-08 22:30:44 +0000

    criando CRUD para Cliente com os endpoints

--- src/main/java/br/com/riume/helpme/model/Cliente.java
+++ src/main/java/br/com/riume/helpme/model/Cliente.java
@@ -2,9 +2,11 @@ package br.com.riume.helpme.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.riume.helpme.dto.ClienteDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
@@ -24,6 +26,17 @@ public class Cliente extends Pessoa{
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();;
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
